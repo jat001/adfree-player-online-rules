eba839c38c2bdde5c607082ed97e2474a78ff28a
Jat <jat@sinosky.org>
2017-07-19 18:11:22 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+Tool Folder
+* Requires: Python 2.7
+* Set your hostUrl in ohostsite.py
+* Edit rules in o[xxx]list.py
+* In oconfiglist.py , -1 = Force Local Mode ; 0 = Do Nothing ; 1 = Force Online Mode
+* Use gen.py to generate useable [xxx]list list.CMD Like: python.exe gen.py
+* Remember, move new files to your rulelist folder.
+
+* gen3.py for Python 3
---
+++ build.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- author: chat@jat.email -*-
+
+import os
+import io
+import sys
+from base64 import b64encode
+
+
+def safe_list_get(l, i, d=None):
+    try:
+        return l[i]
+    except IndexError:
+        return d
+
+
+def main():
+    hostsite = safe_list_get(sys.argv, 1, 'http://127.0.0.1')
+    root_dir = safe_list_get(sys.argv, 2, os.path.dirname(os.path.abspath(__file__)))
+    rules_dir = os.path.join(root_dir, 'rules')
+    build_dir = os.path.join(root_dir, 'build')
+
+    for i in os.listdir(rules_dir):
+        name, ext = i.rsplit('.', 1)
+        if ext != 'json':
+            continue
+
+        with io.open(os.path.join(rules_dir, i), encoding='utf-8') as rule, \
+                io.open(os.path.join(build_dir, name), 'w', encoding='utf-8') as build:
+            build.write(b64encode(rule.read().replace('#hostsite#', hostsite).encode('utf-8')).decode('ascii'))
+
+
+if __name__ == '__main__':
+    main()
