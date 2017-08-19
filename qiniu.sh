b3fc24893fde7a8e0b544b612442f2960c6d9ce7
Jat <jat@sinosky.org>
2017-08-19 16:20:09 +0800

    update ci

---
+++ qiniu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# author: jat@chat.email
+
+./build.py "$HOSTSITE"
+
+./qshell account "$QINIU_AK" "$QINIU_SK"
+sed -i "s/#QINIU_BUCKET#/$QINIU_BUCKET/" .qupload.json
+
+mkdir list
+i=1
+
+while :; do
+    ./qshell qupload -success-list list/success_$i.txt -overwrite-list list/overwrite_$i.txt "$(find build -type f | wc -l)" .qupload.json && break
+    ((i++))
+done
+
+for f in list/*; do
+    sed -ri "s#[^\t]+\t#$HOSTSITE/#" "$f"
+    ./qshell cdnrefresh "$f"
+done
